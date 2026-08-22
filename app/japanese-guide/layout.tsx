9fdec01b0273d309aaf4351a32f3f64d1763e43d
Don <154335034+Steveston147@users.noreply.github.com>
2026-08-23 08:24:37 +0900

    Fix two Japanese mobile heading breaks (#24)
    * Fix Japanese mobile heading line breaks
    
    * Load Japanese mobile heading fix

--- app/japanese-guide/layout.tsx
+++ app/japanese-guide/layout.tsx
@@ -3,6 +3,7 @@ import './proof-gallery.css';
 import './uat-polish.css';
 import './inquiry-form.css';
 import './luxury-uat.css';
+import './mobile-heading-fix.css';
 
 export default function JapaneseGuideLayout({
   children,
