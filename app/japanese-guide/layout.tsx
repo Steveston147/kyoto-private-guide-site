0e9b1677ad8084269c26180a710c19a2039f77b0
Don <154335034+Steveston147@users.noreply.github.com>
2026-08-22 21:30:30 +0900

    refactor: keep Japanese layout presentational

--- app/japanese-guide/layout.tsx
+++ app/japanese-guide/layout.tsx
@@ -3,17 +3,11 @@ import './proof-gallery.css';
 import './uat-polish.css';
 import './inquiry-form.css';
 import './luxury-uat.css';
-import InquiryForm from './InquiryForm';
 
 export default function JapaneseGuideLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      <InquiryForm />
-    </>
-  );
+  return children;
 }
